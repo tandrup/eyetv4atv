efd5f3e166c2efe16b0ae29b0263cba29dbb2ee1
Mads Tandrup <mads@tandrup.org>
2012-06-19 10:32:21 +0200

    Renamed atvHelloWorld folder

---
+++ eyetv4atv/helloWorld/Classes/ApplianceConfig.h
@@ -0,0 +1,43 @@
+//
+//  ApplianceConfig.h
+//  atvHelloWorld
+//
+//  Created by Michael Gile on 8/21/11.
+//  Copyright 2011 Michael Gile. All rights reserved.
+//
+
+
+#import "BackRow.h"
+
+#define APPLIANCE_LOCALIZED_NAME		@"360iDev"
+#define APPLIANCE_NAME					@"360iDev"
+#define APPLIANCE_MODULE_NAME			@"360iDev"
+#define APPLIANCE_KEY					@"360iDev"
+
+#define HW_ID							@"Hello World"
+#define HW_CATEGORY_NAME				@"Hello World"
+#define HW_PREFERRED_ORDER				0
+
+#define T60_ID							@"360iDev"
+#define T60_CATEGORY_NAME				@"360iDev"
+#define T60_PREFERRED_ORDER				1
+
+#define INPUT_EVENTS_ID					@"Input Events"
+#define INPUT_EVENTS_CATEGORY_NAME		@"Input Events"
+#define INPUT_EVENTS_PREFERRED_ORDER	2
+
+#define VIDEO_ID						@"Video Sample"
+#define VIDEO_CATEGORY_NAME				@"Video Sample"
+#define VIDEO_PREFERRED_ORDER			3
+
+#define EAGLCONTROL_ID					@"EAGLControl Example"
+#define EAGLCONTROL_CATEGORY_NAME		@"EAGLControl Example"
+#define EAGLCONTROL_PREFERRED_ORDER		4
+
+#define TEAPOT_ID						@"OpenGL ES Teapot"
+#define TEAPOT_CATEGORY_NAME			@"OpenGL ES Teapot"
+#define TEAPOT_PREFERRED_ORDER			4
+
+
+
+
---
+++ eyetv4atv/helloWorld/Classes/InputEventController.h
@@ -0,0 +1,14 @@
+//
+//  InputEventController.h
+//  atvHelloWorld
+//
+//  Created by Michael Gile on 9/12/11.
+//  Copyright 2011 Michael Gile. All rights reserved.
+//
+
+#import "BRTextEntryController.h"
+
+@interface InputEventController : BRTextEntryController {
+}
+
+@end
